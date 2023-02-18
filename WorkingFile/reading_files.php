38d97d93960790eead62575c02379b9b3a78ac1a
Peter Chen <peterchen2017125@gmail.com>
2023-02-18 17:18:11 +0800

    20230218

--- WorkingFile/reading_files.php
+++ WorkingFile/reading_files.php
@@ -2,7 +2,7 @@
 
 $file = "example.txt";
 if($handle = fopen($file,'r')){
-    $content = fread($handle,10);
+    echo $content = fread($handle,filesize($file));
     fclose($handle);    
 }else{
     echo "The application was not able to write on the file";
