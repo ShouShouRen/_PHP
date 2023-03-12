f7e8d8f1b78f145596f2eb6a6ae742cbfa9d3623
Peter Chen <peterchen2017125@gmail.com>
2023-03-12 16:36:34 +0800

    20230312

--- cms/search.php
+++ cms/search.php
@@ -23,14 +23,43 @@ include("./includes/navigation.php");
                     if(!$search_query){
                         die("QUERY FAILED" .mysqli_error($connection));
                     }
-                    $count = mysqli_num_rows($search_query);
-                    if($count == 0){
-                        echo "<h1> NO RESULT</h1>";
-                    }else{
-                        echo $search;
-                    }
                 }
-            ?>
+                $count = mysqli_num_rows($search_query);
+                if($count == 0){
+                    echo "<h1> NO RESULT</h1>";
+                }else{
+                    // echo $search;
+                    while($row = mysqli_fetch_assoc($search_query)){
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_image = $row['post_image'];
+                        $post_content = $row['post_content'];
+                    }
+                ?>
+
+
+            <h1 class="page-header">
+                Page Heading
+                <small>Secondary Text</small>
+            </h1>
+
+            <!-- First Blog Post -->
+            <h2>
+                <a href="#"><?= $post_title ?></a>
+            </h2>
+            <p class="lead">
+                by <a href="index.php"><?= $post_author ?></a>
+            </p>
+            <p><span class="glyphicon glyphicon-time"></span><?= $post_date ?></p>
+            <hr>
+            <img class="img-responsive" src="./images/<?= $post_image ?>" alt="">
+            <hr>
+            <p><?= $post_content ?></p>
+            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+            <hr>
+            <?php } ?>
 
         </div>
 
