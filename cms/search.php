3fcaa68c68e72fd38111899b8f75da6443b1596b
Peter Chen <peterchen2017125@gmail.com>
2023-03-17 16:21:25 +0800

    20200317

--- cms/search.php
+++ cms/search.php
@@ -11,11 +11,11 @@ include("./includes/navigation.php");
 <!-- Page Content -->
 <div class="container">
 
-    <div class="row">
+  <div class="row">
 
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
-            <?php
+    <!-- Blog Entries Column -->
+    <div class="col-md-8">
+      <?php
                 if(isset($_POST['submit'])){
                     $search = $_POST['search'];
                     $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
@@ -39,36 +39,36 @@ include("./includes/navigation.php");
                 ?>
 
 
-            <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
-            </h1>
+      <h1 class="page-header">
+        Page Heading
+        <small>Secondary Text</small>
+      </h1>
 
-            <!-- First Blog Post -->
-            <h2>
-                <a href="#"><?= $post_title ?></a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php"><?= $post_author ?></a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span><?= $post_date ?></p>
-            <hr>
-            <img class="img-responsive" src="./images/<?= $post_image ?>" alt="">
-            <hr>
-            <p><?= $post_content ?></p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+      <!-- First Blog Post -->
+      <h2>
+        <a href="#"><?= $post_title ?></a>
+      </h2>
+      <p class="lead">
+        by <a href="index.php"><?= $post_author ?></a>
+      </p>
+      <p><span class="glyphicon glyphicon-time"></span><?= $post_date ?></p>
+      <hr>
+      <img class="img-responsive" src="./images/<?= $post_image ?>" alt="">
+      <hr>
+      <p><?= $post_content ?></p>
+      <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-            <hr>
-            <?php } ?>
+      <hr>
+      <?php } ?>
 
-        </div>
+    </div>
 
-        <!-- Blog Sidebar Widgets Column -->
-        <?php
+    <!-- Blog Sidebar Widgets Column -->
+    <?php
             include("./includes/sidebar.php");
         ?>
-    </div>
-    <!-- /.row -->
+  </div>
+  <!-- /.row -->
 
-    <hr>
-    <?php include("./includes/footer.php"); ?>
+  <hr>
+  <?php include("./includes/footer.php"); ?>
